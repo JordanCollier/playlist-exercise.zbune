786d45fef993edd6f07cdfcb58a6f263bb2a023f
Jordan Collier <jordanicollier@gmail.com>
2015-03-24 13:34:41 -0600

    added Welsey's answer

--- playlist.rb
+++ playlist.rb
@@ -1,65 +1,115 @@
-require 'pry-byebug'
-require_relative 'song'
 class Playlist
-
-  attr_accessor :name, :playlist
-
+  attr_reader :name, :songs
 
   def initialize(name)
     @name = name
-    @playlist = []
-    @current_song = nil
-    @song_number = 0
+    @songs = []
   end
 
   def is_empty?
-    if @playlist.empty?
-      true
-    else
-      false
-    end
+    @songs.empty?
   end
 
   def add_song(song)
-    @playlist << song
-  end
-
-  def songs
-    @playlist
+    @songs << song
   end
 
   def remove_song(song)
-    @playlist.delete(song)
+    @songs.delete(song)
   end
 
   def song_names
-    @playlist.map do |song|
-      song.name
-    end
+    @songs.map(&:name)
   end
 
   def swap(song1, song2)
-    @playlist[@playlist.index(song1)], @playlist[@playlist.index(song2)] = @playlist[@playlist.index(song2)], @playlist[@playlist.index(song1)]
+    index1 = @songs.index(song1)
+    index2 = @songs.index(song2)
+    @songs[index1], @songs[index2] = song2, song1
   end
 
   def total_length
-    @playlist.inject(0) do |sum, song|
-      sum += song.length
-    end
-  end
-
-  def now_playing
-    @current_song
+    @songs.reduce(0) {|memo, song| memo + song.length}
   end
 
   def play
-    @current_song = @playlist[@song_number]
+    @playing_index = 0
+  end
+
+  def now_playing
+    @songs[@playing_index] if @playing_index
   end
 
   def next
-    @song_number += 1
-    self.play
+    @playing_index += 1
   end
 
-
 end
+
+#-----------------------MY SOLUTION----------------------------
+
+# require 'pry-byebug'
+# require_relative 'song'
+# class Playlist
+#
+#   attr_accessor :name, :playlist
+#
+#
+#   def initialize(name)
+#     @name = name
+#     @playlist = []
+#     @current_song = nil
+#     @song_number = 0
+#   end
+#
+#   def is_empty?
+#     if @playlist.empty?
+#       true
+#     else
+#       false
+#     end
+#   end
+#
+#   def add_song(song)
+#     @playlist << song
+#   end
+#
+#   def songs
+#     @playlist
+#   end
+#
+#   def remove_song(song)
+#     @playlist.delete(song)
+#   end
+#
+#   def song_names
+#     @playlist.map do |song|
+#       song.name
+#     end
+#   end
+#
+#   def swap(song1, song2)
+#     @playlist[@playlist.index(song1)], @playlist[@playlist.index(song2)] = @playlist[@playlist.index(song2)], @playlist[@playlist.index(song1)]
+#   end
+#
+#   def total_length
+#     @playlist.inject(0) do |sum, song|
+#       sum += song.length
+#     end
+#   end
+#
+#   def now_playing
+#     @current_song
+#   end
+#
+#   def play
+#     @current_song = @playlist[@song_number]
+#   end
+#
+#   def next
+#     @song_number += 1
+#     self.play
+#   end
+#
+#
+# end
